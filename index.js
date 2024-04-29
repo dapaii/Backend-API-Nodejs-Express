6d2e013c65061bf5b7db20e5ebcdd2892fa96a3a
ecowhite <ecowhite.ac.id@gmail.com>
2024-04-29 16:13:22 +0700

    part 2

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 // Routes / URL / endpoint utama method GET
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.send('Home')
 })
 
 // Routes / URL / endpoint admin method GET
-app.get('/admin', (req, res)=>{
-    res.send("admin")
+app.get('/admin', (req, res) => {
+  console.log({ urlParams: req.query})
+  res.send("admin")
 })
 
-app.post('/login', (req, res)=>{
-    if(req.name === "admin"){
-        res.send('login succes')
-    }
+app.post('/login', (req, res) => {
+  console.log({ requestFromOutside : req.body})
+  res.send('login succes')
+})
+
+app.put('/LupaPassword', (req, res)=>{
+  console.log({ updateData: req.body})
+  res.send("update Berhasil")
 })
 
 app.listen(port, () => {
