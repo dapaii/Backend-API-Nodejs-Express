e19bc49bc8afdd2277925de882b208660f511594
ecowhite <ecowhite.ac.id@gmail.com>
2024-04-30 17:10:57 +0700

    part5

--- index.js
+++ index.js
@@ -10,28 +10,88 @@ app.use(bodyParser.json())
 
 // ROUTE PATH START
 app.get('/', (req, res) => {
-  response(200, "ini data", "ini message", res)
+  response(200, "API READY", "Succes", res)
 })
 
 app.get('/mahasiswa', (req, res) => {
-  response(200, "ini data", "mahasiswa get list", res)
+
+  const sql = "SELECT * FROM mahasiswa"
+
+  db.query(sql, (err, fields)=>{
+    if(err) throw err
+    response(200, fields, "mahasiswa get list", res)
+  })
+
 })
 
 app.get('/mahasiswa/:nim', (req, res) => {
+
   const nim = req.params.nim
-  response(200, "ini data", `list mahasiswa by id : ${nim}`, res)
+  const sql = `SELECT * FROM mahasiswa WHERE nim = ${nim}`
+
+ db.query(sql, (err, fields)=>{
+  if(err) throw err
+   response(200, fields, `list mahasiswa by id : ${nim}`, res)
+ })
+
 })
 
 app.post('/mahasiswa', (req, res) => {
-  response(200, "ini data", "POST", res)
+
+  const { nim, nama_lengkap, kelas, alamat } = req.body
+  const sql = `INSERT INTO mahasiswa (nim, nama_lengkap, kelas, alamat) VALUES (${nim}, '${nama_lengkap}', '${kelas}', '${alamat}')`
+
+  db.query(sql, (err, fields)=>{
+    if(err) response(500, "invalid", "error", res)
+    if(fields?.affectedRows){
+      const data = {
+        isSuccess: fields.affectedRows,
+        id: fields.insertId
+      }
+      response(200, data, "Add Data Successfuly", res)
+    }else{
+      response(404, "Invalid Add Data", "error", res)
+    }
+  })
+
 })
 
 app.put('/mahasiswa', (req, res) => {
-  response(200, "ini data", "PUT / UPDATE DATA", res)
+
+  const {nim, nama_lengkap, kelas, alamat} = req.body
+  const sql = `UPDATE mahasiswa SET nama_lengkap = '${nama_lengkap}', kelas = '${kelas}', alamat = '${alamat}' WHERE nim = '${nim}'`
+
+  db.query(sql, (err, fields)=>{
+    if(err) response(500, "invalid", "error", res)
+    if(fields?.affectedRows){
+      const data = {
+        isSuccess: fields.affectedRows,
+        message: fields.message
+      }
+      response(200, data, "Updated Data Successfuly", res)
+    }else{
+      response(404, "user not found ", "error", res)
+    }
+  })
+
 })
 
 app.delete('/mahasiswa', (req, res) => {
-  response(200, "ini data", "DELETE", res)
+  
+  const {nim} = req.body
+  const sql = `DELETE FROM mahasiswa WHERE nim = ${nim}`
+
+  db.query(sql, (err, fields)=>{
+    if(err)response(200, "invalid", "error", res)
+    if(fields?.affectedRows){
+      const data = {
+        isSuccess : fields.affectedRows
+      }
+      response(200, data, "Deleted Data Successfuly ", res)
+    }else{
+      response(404, "user not found", "error", res)
+    }
+  })
 })
 // ROUTE PATH END
 
