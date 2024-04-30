e948f17c464f8764ea3e3ec0e8343c0405d8d760
ecowhite <ecowhite.ac.id@gmail.com>
2024-04-30 13:17:33 +0700

    part3

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+// untuk connection ke->db 
+const db = require('./connection')
+
+const response = require('./response')
 
 app.use(bodyParser.json())
 
 // Routes / URL / endpoint utama method GET
 app.get('/', (req, res) => {
-  res.send('Home')
+
+  const sql = "SELECT * FROM mahasiswa"
+
+  db.query(sql, (error, result) =>{
+    response(200, result, "get all data from mahasiswa", res)
+  })
 })
 
 // Routes / URL / endpoint admin method GET
-app.get('/admin', (req, res) => {
-  console.log({ urlParams: req.query})
-  res.send("admin")
+app.get('/find', (req, res) => {
+   
+  const sql = `SELECT nama_lengkap FROM mahasiswa WHERE nim = ${req.query.nim}`
+
+  db.query(sql, (error, result) =>{
+    response(200,  result, "find mahasiswa name", res)
+  })
 })
 
 app.post('/login', (req, res) => {
